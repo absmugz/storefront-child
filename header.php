d6510c40277a1effabd11624a16edc4898810985
Absolom Mugwagwa <absmugz09@gmail.com>
2017-06-19 14:18:03 +0000

    customising storefront

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( 'body-wrapper static' ); ?>>
+  <div class="se-pre-con"></div>
+  <div class="main-wrapper">
+
+
+
+	<header  id="pageTop" class="header" style="<?php storefront_header_styles(); ?>">
+		
+		<!-- TOP INFO BAR -->
+      <div class="top-info-bar">
+        <div class="container">
+          <div class="top-bar-right">
+            <ul class="list-inline">
+              <li><a href=""><i class="fa fa-envelope" aria-hidden="true"></i> info@yourdomain.com</a></li>
+              <li><span><i class="fa fa-phone" aria-hidden="true"></i>+1 234 567 8900</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+	
+
+			<?php
+			/**
+			 * Functions hooked into storefront_header action
+			 *
+			 * @hooked storefront_skip_links                       - 0
+			 * @hooked storefront_social_icons                     - 10
+			 * @hooked storefront_site_branding                    - 20
+			 * @hooked storefront_secondary_navigation             - 30
+			 * @hooked storefront_product_search                   - 40
+			 * @hooked storefront_primary_navigation_wrapper       - 42
+			 * @hooked storefront_primary_navigation               - 50
+			 * @hooked storefront_header_cart                      - 60
+			 * @hooked storefront_primary_navigation_wrapper_close - 68
+			 */
+			do_action( 'storefront_header' ); ?>
+
+	
+	</header><!-- #masthead -->
+
+	<?php
+	/**
+	 * Functions hooked in to storefront_before_content
+	 *
+	 * @hooked storefront_header_widget_region - 10
+	 */
+	do_action( 'storefront_before_content' ); ?>
+
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		<?php
+		/**
+		 * Functions hooked in to storefront_content_top
+		 *
+		 * @hooked woocommerce_breadcrumb - 10
+		 */
+		do_action( 'storefront_content_top' );
