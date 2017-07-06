0ba7f647f72d7b8298efa488f8d444187bc2ec36
Absolom Mugwagwa <absmugz09@gmail.com>
2017-07-06 14:59:15 +0000

    Now We Can Use HTML Tags broken

--- template-homepage.php
+++ template-homepage.php
@@ -55,59 +55,7 @@ $conf_sender = 'Allure Hair & Beauty <no-reply@allurestudio.co.za>';
 $msg = $name . ",\n\nThank you for your recent enquiry. A member of our 
 team will respond to your message as soon as possible.";
 
-
-   
-$message .= "<table width='100%' bgcolor='#e0e0e0' cellpadding='0' cellspacing='0' border='0'>";
-   
-$message .= "<tr><td>";
-   
-$message .= "<table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#fff; font-family:Verdana, Geneva, sans-serif;'>";
-    
-$message .= "<thead>
-  <tr height='80'>
-  <th colspan='4' style='background-color:#f5f5f5; border-bottom:solid 1px #bdbdbd; font-family:Verdana, Geneva, sans-serif; color:#333; font-size:34px;' >Coding Cage</th>
-  </tr>
-             </thead>";
-    
-$message .= "<tbody>
-             <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
-       <td style='background-color:#00a2d1; text-align:center;'><a href='http://www.codingcage.com/search/label/PDO' style='color:#fff; text-decoration:none;'>PDO</a></td>
-       <td style='background-color:#00a2d1; text-align:center;'><a href='http://www.codingcage.com/search/label/jQuery' style='color:#fff; text-decoration:none;'>jQuery</a></td>
-       <td style='background-color:#00a2d1; text-align:center;'><a href='http://www.codingcage.com/search/label/PHP OOP' style='color:#fff; text-decoration:none;' >PHP OOP</a></td>
-       <td style='background-color:#00a2d1; text-align:center;'><a href='http://www.codingcage.com/search/label/MySQLi' style='color:#fff; text-decoration:none;' >MySQLi</a></td>
-       </tr>
-      
-       <tr>
-       <td colspan='4' style='padding:15px;'>
-       <p style='font-size:20px;'>Hi' ".$name.",</p>
-       <hr />
-       <p style='font-size:25px;'>Sending HTML eMaile using PHPMailer</p>
-       <img src='https://4.bp.blogspot.com/-rt_1MYMOzTs/VrXIUlYgaqI/AAAAAAAAAaI/c0zaPtl060I/s1600/Image-Upload-Insert-Update-Delete-PHP-MySQL.png' alt='Sending HTML eMail using PHPMailer in PHP' title='Sending HTML eMail using PHPMailer in PHP' style='height:auto; width:100%; max-width:100%;' />
-       <p style='font-size:15px; font-family:Verdana, Geneva, sans-serif;'>".$message.".</p>
-       </td>
-       </tr>
-      
-       <tr height='80'>
-       <td colspan='4' align='center' style='background-color:#f5f5f5; border-top:dashed #00a2d1 2px; font-size:24px; '>
-       <label>Coding Cage On : 
-              <a href='https://facebook.com/CodingCage' target='_blank'><img style='vertical-align:middle' src='https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-facebook-m.png' /></a>
-       <a href='https://twitter.com/CodingCage' target='_blank'><img style='vertical-align:middle' src='https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-twitter-m.png' /></a>
-       <a href='https://plus.google.com/+PradeepKhodked' target='_blank'><img style='vertical-align:middle' src='https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-googleplus-m.png' /></a>
-       <a href='https://feeds.feedburner.com/cleartutorials' target='_blank'><img style='vertical-align:middle' src='https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-rss-m.png' /></a>
-              </label>
-       </td>
-       </tr>
-      
-              </tbody>";
-    
-$message .= "</table>";
-   
-$message .= "</td></tr>";
-$message .= "</table>";
-   
-$message .= "</body></html>";
-
-mail( $email, $conf_subject, $message, 'From: ' . $conf_sender );
+mail( $email, $conf_subject, $msg, 'From: ' . $conf_sender );
 
 
   if(!$human == 0){
